83ff33b0d1940338f92cb004b8cdb8d84e26bf65
JoseSleiter <josesleiter@gmail.com>
2023-03-09 12:12:25 -0400

    Feature: Styles to responsive Pc and Tablet movies

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -3,12 +3,13 @@ import withoutResults from './../mocks/getMovies-error.json'
 import { ListMovies } from './listMovies';
 import { NotFoundMovies } from './notFoundMovies';
 import { useEffect, useState } from 'react';
+import { mappedMovies } from '../helpers/mapped/mappedMovies'
 
 export const Movies = () => {
     const [movies, setMovies] = useState([]);
     const [hasMovies, setHasMovies] = useState(false)
     useEffect(() => {
-        setMovies(withResults.Search)
+        setMovies(mappedMovies(withResults.Search))
         setHasMovies(withResults.Search.length > 0)
     }, []);
 
